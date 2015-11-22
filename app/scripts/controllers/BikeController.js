b584665837292e2055095e53db8609df0785198d
Alex <alex.lubneuski@gmail.com>
2015-11-21 22:44:27 -0500

    Fixed issues with google maps being slow

--- app/scripts/controllers/BikeController.js
+++ app/scripts/controllers/BikeController.js
@@ -19,16 +19,16 @@ var ctrs = angular.module('BikeControllers',[]);
                                            function($scope, $routeParams, bikeService) {
       bikeService.query(function(data) {
          //Iterate throgh the data and pass
+         var myLatLng = new google.maps.LatLng(39.9522, -75.1639);
+         var mapOptions = {
+                 center: myLatLng,
+                 zoom: 12
+             };
+
+         var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
           angular.forEach( data.features, function (value){
-                var myLatLng = new google.maps.LatLng(value.geometry.coordinates[1], value.geometry.coordinates[0]);
-                var mapOptions = {
-                        center: myLatLng,
-                        zoom: 12
-                    };
 
-              var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-/*
               if (value.properties.kioskId == $routeParams.kioskId) {
                 $scope.kioskData = value;
                 var myLatLng = new google.maps.LatLng(value.geometry.coordinates[1], value.geometry.coordinates[0]);
@@ -37,7 +37,8 @@ var ctrs = angular.module('BikeControllers',[]);
                         zoom: 12
                     };
                 var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-                var contentForInfoWindow = '<div>Available Bikes:' + value.properties.bikesAvailable + '</div>'
+                var contentForInfoWindow = '<div>Available Bikes:' + value.properties.bikesAvailable + '</div>' +
+                '<div>Station Name: ' + value.properties.
                 var infowindow = new google.maps.InfoWindow({
                     content: contentForInfoWindow
                 });
@@ -45,16 +46,39 @@ var ctrs = angular.module('BikeControllers',[]);
                 var marker = new google.maps.Marker({
                     position: myLatLng,
                     map: map,
+                    animation: google.maps.Animation.DROP,
                     title: value.properties.addressStreet
                 });
                 marker.addListener('click', function() {
                     infowindow.open(map,marker);
                 });
               }
-*/
+           });
+      });
+  }]);
+
+  ctrs.controller('BikeMainController', ['$scope','$routeParams','bikeService',
+                                           function($scope, $routeParams, bikeService) {
+      bikeService.query(function(data) {
+         //Iterate throgh the data and pass
+         //Iterate throgh the data and pass
+         var myLatLng = new google.maps.LatLng(39.9522, -75.1639);
+         var mapOptions = {
+                 center: myLatLng,
+                 zoom: 12
+             };
+
+         var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+          angular.forEach( data.features, function (value){
+            var contentForInfoWindow = '<div>Available Bikes:' + value.properties.bikesAvailable + '</div>'
+            var infowindow = new google.maps.InfoWindow({
+                content: contentForInfoWindow
+            });
                     var marker = new google.maps.Marker({
                     position: new google.maps.LatLng(value.geometry.coordinates[1], value.geometry.coordinates[0]),
                     map: map,
+                    animation: google.maps.Animation.DROP,
                     title: value.properties.addressStreet
                 });
                 marker.addListener('click', function() {
@@ -62,5 +86,6 @@ var ctrs = angular.module('BikeControllers',[]);
                 });
 
            });
+
       });
   }]);
